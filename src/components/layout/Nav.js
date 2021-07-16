df3114420866cf57a2b6c482ad3fff6da5849b13
Farhad Ullah Rezwan <53939630+Farhad-Rezwan@users.noreply.github.com>
2021-07-16 10:31:39 +1000

    React user logout

--- src/components/layout/Nav.js
+++ src/components/layout/Nav.js
@@ -1,5 +1,21 @@
 import { NavLink } from "react-router-dom";
+import { useContext } from "react";
+
+import { LoginContext } from "../../utilities/reducers";
+import AuthService from "../../services/AuthService";
+
+const authService = new AuthService();
+
 const Nav = () => {
+  const {
+    login: { user },
+  } = useContext(LoginContext);
+
+  const logout = () => {
+    authService.logout();
+    window.location.href = "/";
+  };
+
   return (
     <nav className="bg-gradient-to-r from-blue-500 via-blue-400 to-blue-500">
       <div className="p-2 text-xl text-white grid grid-cols-2 flex justify-between">
@@ -10,18 +26,27 @@ const Nav = () => {
             </NavLink>
           </div>
         </div>
-        <div className="flex justify-end">
-          <div className="p-2 text-xl">
-            <NavLink to="/login" exact>
-              Login
-            </NavLink>
+        {!user && (
+          <div className="flex justify-end">
+            <div className="p-2 text-xl">
+              <NavLink to="/login" exact>
+                Login
+              </NavLink>
+            </div>
+            <div className="p-2 text-xl">
+              <NavLink to="/signup" exact>
+                Sign up
+              </NavLink>
+            </div>
           </div>
-          <div className="p-2 text-xl">
-            <NavLink to="/signup" exact>
-              Sign up
-            </NavLink>
+        )}
+        {user && (
+          <div className="flex justify-end">
+            <button className="p-2 text-xl" onClick={logout}>
+              Logout
+            </button>
           </div>
-        </div>
+        )}
       </div>
     </nav>
   );
