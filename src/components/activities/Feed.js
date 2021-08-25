56203e5625864b1898c1e2a6a0d3c6a1956fae0d
Farhad Ullah Rezwan <53939630+Farhad-Rezwan@users.noreply.github.com>
2021-08-25 18:15:52 +1000

    Sort feed according to time

--- src/components/activities/Feed.js
+++ src/components/activities/Feed.js
@@ -1,6 +1,7 @@
 import { useQuery } from 'react-query';
 import { activitiesQuery } from '../../utilities/queries';
 import CreateStrategyFeed from './NewStrategyFeed';
+import { compareDesc } from 'date-fns';
 
 const Feed = () => {
   const { data: activities } = useQuery(['activities'], () => activitiesQuery(), {
@@ -9,9 +10,11 @@ const Feed = () => {
 
   if (!activities) return <></>;
 
+  const sortedActivities = activities.sort((a, b) => compareDesc(new Date(a.createdAt), new Date(b.createdAt)));
+
   return (
     <div className="bg-white shadow overflow-hidden sm:rounded-lg m-4">
-      {activities.map(activity => (
+      {sortedActivities.map(activity => (
         <>{activity.event === 'CreateStrategy' && <CreateStrategyFeed activity={activity} />}</>
       ))}
     </div>
