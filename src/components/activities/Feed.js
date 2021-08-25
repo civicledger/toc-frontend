4922e5585f64aecd99b5be4f0650ea6136b1a39a
Farhad Ullah Rezwan <53939630+Farhad-Rezwan@users.noreply.github.com>
2021-08-25 17:51:02 +1000

    Strategy feed view created

--- src/components/activities/Feed.js
+++ src/components/activities/Feed.js
@@ -1,24 +1,20 @@
 import { useQuery } from 'react-query';
 import { activitiesQuery } from '../../utilities/queries';
+import CreateStrategyFeed from './NewStrategyFeed';
 
 const Feed = () => {
-  const { data: activities } = useQuery('activities', activitiesQuery, {
+  const { data: activities } = useQuery(['activities'], () => activitiesQuery(), {
     keepPreviousData: true,
   });
 
   if (!activities) return <></>;
 
   return (
-    <>
-      <div>
-        <h2 className="text-2xl font-bold leading-7 text-gray-900 sm:text-3xl mb-10">Activity Feed</h2>
-        {activities.map(activity => (
-          <div key={activity.id} className="mb-10 border shadow p-10">
-            {activity.name}
-          </div>
-        ))}
-      </div>
-    </>
+    <div className="bg-white shadow overflow-hidden sm:rounded-lg m-4">
+      {activities.map(activity => (
+        <>{activity.event === 'CreateStrategy' && <CreateStrategyFeed activity={activity} />}</>
+      ))}
+    </div>
   );
 };
 
--- src/utilities/format.js
+++ src/utilities/format.js
@@ -1,11 +1,16 @@
-import { format, parseISO } from "date-fns";
+import { format, parseISO } from 'date-fns';
 
-export const shortDate = (date) => {
+export const shortDate = date => {
   if (!date) date = new Date();
-  return format(date, "dd MMM yyyy");
+  return format(date, 'dd MMM yyyy');
 };
 
-export const shortRawDate = (dateString) => {
-  if (!dateString) return "";
-  return format(parseISO(dateString), "dd MMM yyyy");
+export const shortTime = date => {
+  if (!date) date = new Date();
+  return format(date, 'h:mm aaa');
+};
+
+export const shortRawDate = dateString => {
+  if (!dateString) return '';
+  return format(parseISO(dateString), 'dd MMM yyyy');
 };
